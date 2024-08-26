12f2cc9fa83c9395060c1b736cc3ef8bcce5c716
wh1tepr1nc3 <amamamania@mail.ru>
2024-08-26 22:19:55 +0500

    Add files via upload

--- module_5_4.py
+++ module_5_4.py
@@ -3,16 +3,16 @@ class House:
     houses_history = []
 
     def __new__(cls, *args, **kwargs):
-        instance = object.__new__(cls)
+        obj = object.__new__(cls)
         args = args[0]
         cls.houses_history.append(args)
-        return instance
+        return obj
 
     def __init__(self, name, number_of_floors):
         self.name = name
         self.number_of_floors = number_of_floors
-        if isinstance(number_of_floors, House):
-            self.houses_history = number_of_floors.append()
+        if isinstance(number_of_floors, int):
+            self.houses_history = number_of_floors
 
     # def go_to(self, new_floor):
     #     if 0 < new_floor <= self.number_of_floors:
@@ -22,7 +22,7 @@ class House:
     #         print('Такого этажа не существует')
 
     def __del__(self):
-        return print(f'{self.name} снесён, но он останется в истории')
+        print(f'{self.name} снесён, но он останется в истории')
 
     def __len__(self):
         return self.number_of_floors
@@ -30,41 +30,47 @@ class House:
     def __str__(self):
         return f'Название: "{self.name}", кол-во этажей: {self.number_of_floors}.'
 
-    #
-    # def __eq__(self, other):
-    #     return isinstance(other, int)
-    #
-    # def __lt__(self, other):
-    #     if not isinstance(other, int):
-    #         return self.number_of_floors < other.number_of_floors
-    #
-    # def __le__(self, other):
-    #     if not isinstance(other, int):
-    #         return self.number_of_floors <= other.number_of_floors
-    #
-    # def __gt__(self, other):
-    #     if not isinstance(other, int):
-    #         return self.number_of_floors > other.number_of_floors
-    #
-    # def __ge__(self, other):
-    #     if not isinstance(other, int):
-    #         return self.number_of_floors >= other.number_of_floors
-    #
-    # def __ne__(self, other):
-    #     if not isinstance(other, int):
-    #         return self.number_of_floors != other.number_of_floors
-    #
-    # def __add__(self, value):
-    #     self.number_of_floors += value
-    #     return self
-    #
-    # def __iadd__(self, other):
-    #     if isinstance(other, int) or isinstance(other, House):
-    #         return self + other
-    #
-    # def __radd__(self, other):
-    #     if isinstance(other, int) or isinstance(other, House):
-    #         return self + other
+    def __eq__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
+        return False
+
+    def __lt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
+        return NotImplemented
+
+    def __le__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
+        return NotImplemented
+
+    def __gt__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
+        return NotImplemented
+
+    def __ge__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
+        return NotImplemented
+
+    def __ne__(self, other):
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
+        return True
+
+    def __add__(self, value):
+        if isinstance(value, int):
+            return House(self.name, self.number_of_floors + value)
+        return NotImplemented
+
+    def __radd__(self, value):
+        return self.__add__(value)
+
+    def __iadd__(self, other):
+        if isinstance(other, int) or isinstance(other, House):
+            return self + other
 
 
 h1 = House('ЖК Эльбрус', 10)
@@ -82,10 +88,9 @@ print(House.houses_history)
 
 # print(h1)
 # print(h2)
-# print(h3)
 #
 # print(h1 == h2)  # __eq__
-#
+
 # h1 = h1 + 10  # __add__
 # print(h1)
 # print(h1 == h2)
